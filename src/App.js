7d130a8f4bad057f9f103a236cefb83b14153b91
yejinlong <1244338192@qq.com>
2020-09-29 01:01:40 +0800

    初始化

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import ReactDOM from "react-dom";
+import { Button } from "element-react";
+import "element-theme-default";
 
 function App() {
   return (
@@ -8,17 +12,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit <code> src / App.js </code> and save to reload.{" "}
+        </p>{" "}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Learn React{" "}
+        </a>{" "}
+      </header>{" "}
     </div>
   );
 }
